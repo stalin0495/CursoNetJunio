c07b83eafae045e8b31f83dc232331193719d42b
scondolo <scondolo@pichincha.com>
2022-07-01 12:21:56 -0500

    Fase 3
    SE desarrollo la fase 3

---
+++ evaluacion-practica/Stalin Condolo/Curso.Biblioteca/Curso.Biblioteca.Infraestructura/Context/BibliotecaDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Curso.Biblioteca.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso.Biblioteca.Infraestructura.Context
+{
+    public class BibliotecaDbContext : DbContext
+    {
+        public BibliotecaDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Autor> Autores { get; set; }
+        public DbSet<Editorial> Editoriales { get; set; }
+        public DbSet<Libro> Libros { get; set; }
+
+
+    }
+}
